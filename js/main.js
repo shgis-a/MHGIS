e8ffeba48351b7ffd2db2180927674f22cdf21cb
virgilxw <reubenwxw@gmail.com>
2021-01-15 19:02:24 +0800

    add and remove base layer operational

--- js/main.js
+++ js/main.js
@@ -1,97 +1,57 @@
-faded_buffer = {}
+var map = L.map('map', {
+	preferCanvas: true
+}).setView([4.2, 108.00], 6)
 
-function filter_state(key, operation) {
-	// Helper function to identify what entries to fade out (de-selected). Takes three arguments; Firstly, "key" is the field you want to filter. "Operation" has two options: "1" removes items from the list (i.e. un-fades points) and "2" add items to the list (i.e. fades out points)
+all_layers = L.layerGroup()
 
-	// Input validation
-	valid_inputs_state = ["Johor", "Kedah", "Kelantan", "Malacca", "NegeriSembilan", "Pahang", "Penang", "Perak", "Perlis", "Sabah", "独立中学", "Sarawak", "Selangor", "Terengganu", "KualaLumpur", "Labuan", "Putrajaya"];
-
-	valid_inputs_category = ["宗教", "地缘", "综合", "青年妇女", "文教", "体育联谊", "慈善福利", "独立中学", "血缘", "业缘"];
-
-	category = ""
-
-	if (valid_inputs_state.includes(key)) {
-		category = "Region"
-	} else if (valid_inputs_category.includes(key)) {
-		category = "Category"
-	} else {
-		console.error("Entry '", key, "' is invalid");
-		return 1;
-	}
-
-	if (operation == 1) {
-		$.getJSON("./json/MHGIS Masterlist.json", function (data) {
-			data.forEach(function (entry, index) {
-				if (entry[category] == key) {
-
-					if (faded_buffer[entry["UID"]] > 1) {
-						faded_buffer[entry["UID"]] = faded_buffer[entry["UID"]] - 1
-					} else if (faded_buffer[entry["UID"]] == 1) {
-						delete faded_buffer[entry["UID"]]
-					}
-				}
-			})
-		})
-	} else if (operation == 2) {
-		$.getJSON("./json/MHGIS Masterlist.json", function (data) {
-			data.forEach(function (entry, index) {
-				if (entry[category] == key) {
-
-					if (entry["UID"] in faded_buffer) {
-						faded_buffer[entry["UID"]] = faded_buffer[entry["UID"]] + 1
-					} else {
-						faded_buffer[entry["UID"]] = 1
-					}
-				}
-			})
-			
-			console.log(map)
-		})
-	}
-}
-
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2hnaXMta2VubmV0aGRlYW4iLCJhIjoiY2tqMTBpOHl0MDI0YzJ5c2IzOHMyM2V4eCJ9.DFNMWEGdVJkBh9mS2OkrbA';
-
-var map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/shgis-kennethdean/ckj4dgeskcfpi19qm1bd2tu23', // stylesheet location
-	center: [109.455, 4.109], // starting position [lng, lat]
-	zoom: 5 // starting zoom
-});
-
-map.on('click', function (e) {
-	var features = map.queryRenderedFeatures(e.point, {
-		layers: ['mhgis-base'] // replace this with the name of the layer
-	});
-
-	if (!features.length) {
-		return;
-	}
-
-	console.log(features)
-	var feature = features[0];
-
-	var popup = new mapboxgl.Popup({
-			offset: [0, -15]
-		})
-		.setLngLat(feature.geometry.coordinates)
-		.setHTML('<div class="popup"><h3>' + feature.properties.Name_CH + '</h3><h4>' + feature.properties.Name_ML + '</h4><h4>' + feature.properties.Name_EN + '</h4><ul><li>' + 'State: ' + feature.properties.Region + '</li><li>' + 'Category: ' + feature.properties.Category + '</li><li>' + 'Address: ' + feature.properties.Location + '</li><li>' + 'Source: 《马来西亚华团总名册》pg.' + feature.properties.PageNumber + '</li></ul></div>')
-		.addTo(map);
-});
-
-// Collaspe button
-$('span.collapse').on('click', function () {
-	$(this).parent().find('ul').slideToggle();
-});
+L.tileLayer('https://api.mapbox.com/styles/v1/shgis-kennethdean/ckj4dgeskcfpi19qm1bd2tu23/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2hnaXMta2VubmV0aGRlYW4iLCJhIjoiY2tqMTBpOHl0MDI0YzJ5c2IzOHMyM2V4eCJ9.DFNMWEGdVJkBh9mS2OkrbA', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 $(document).ready(function () {
-	$("input").change(function () {
 
+	//Open sidebar to intro page
+	var sidebar = L.control.sidebar({
+		autopan: true, // whether to maintain the centered map point when opening the sidebar
+		closeButton: true, // whether t add a close button to the panes
+		container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+		position: 'left', // left or right
+	}).addTo(map);
 
-		if ($(this).is(":checked")) {
-			filter_state($(this).attr('id'), 1)
-		} else {
-			filter_state($(this).attr('id'), 2)
+	sidebar.open('home');
+
+	var renderer = L.canvas({
+		padding: 0.5
+	});
+
+	// Load points as circle markers
+	$.getJSON("./json/MHGIS Masterlist.json", function (data) {
+		data.forEach(function (item, index) {
+
+			latlng = [item.latitude, item.longitude]
+			item["UID"] = L.circleMarker(latlng, {
+				renderer: renderer,
+				radius: 5,
+				fillColor: "#fbb4ae",
+				weight: 1,
+				color: "#000000",
+				opacity: 0.2
+			}).addTo(map).on("click", function () {
+				console.log(item)
+			})
+
+			all_layers.addLayer(item["UID"])
+		})
+	})
+
+	sidebar.on('content', function (e) {
+
+		console.log(e.id)
+		if (e.id == "home") {
+			map.addLayer(all_layers)
+
+		} else if (e.id == "roster") {
+			map.removeLayer(all_layers)
 		}
 	})
 })
